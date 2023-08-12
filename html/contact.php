88637c8514c3325aa033d0f6e76be987ee3fd16a
bramderaeve <contact@doomhowl-interactive.com>
2023-08-12 11:22:53 +0200

    meer e-mail

--- html/contact.php
+++ html/contact.php
@@ -12,7 +12,8 @@ include_once '/var/www/php/common.php';
             <?php include_once("navbar.php") ?>
             <main>
                 <h1>Contact</h1>
-                <p>Email: <a href="mailto:bram.deraeve@telenet.be">bram.deraeve@telenet.be</a></p>
+                <p>e-mail: <a href="mailto:bram.deraeve@telenet.be">bram.deraeve@telenet.be</a></p>
+                <p>HOWEST e-mail: <a href="mailto:bram.deraeve@student.howest.be">bram.deraeve@student.howest.be</a></p>
             </main>
         </div>
         <?php include_once("photo.php") ?>
